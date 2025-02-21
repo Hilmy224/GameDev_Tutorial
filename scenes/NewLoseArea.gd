35253331a3e186dc60272e91e597a12e3f461386
Hilmy224 <the.real.a.pacifist@gmail.com>
2025-02-21 09:35:01 +0700

    Script Tweaked

--- scenes/NewLoseArea.gd
+++ scenes/NewLoseArea.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 func _on_ObjectiveArea_body_entered(body: CharacterBody2D):
 	if (body.name == "CharacterBody2D"):
-		print("Reached objective!")
+		print("You Died")
 		get_tree().reload_current_scene()
